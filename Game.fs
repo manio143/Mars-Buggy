40fd42563fbd2657dc3624b07faa12f3013af57e
Marian Dziubiak <marian.dziubiak@gmail.com>
2017-03-12 10:11:47 +0100

    Add Scene abstraction (class based)

--- Game.fs
+++ Game.fs
@@ -2,21 +2,50 @@
 
 open SadConsole.Consoles
 open SadConsole.Game
+open SadConsole.Input
 open SadConsole
 
 open ConsoleObject
 
 open Microsoft.Xna.Framework
 
-let welcomeScreen = 
-    createWithAnimationFromFile 20 4 "welcomeScreen"
-    <| {defaultSurface with Foreground = Color.OrangeRed}
+[<AbstractClass>]
+type Scene() =
+    abstract member ConsoleObjects: unit -> GameObject list
+    abstract member Update: unit -> unit
+    abstract member ProcessKeyboard: KeyboardInfo -> Scene option
 
+    default this.Update() = ()
+
+type StartScene() =
+    inherit Scene()
+
+    let welcomeScreen = 
+        createWithAnimationFromFile 20 4 "welcomeScreen"
+        <| {defaultSurface with Foreground = Color.OrangeRed}
+
+    let instructions =
+        createWithAnimation 1 24 [|"Press [Space] to start, [Q] to quit, [H] for help."|]
+        <| defaultSurface
+
+    override this.ConsoleObjects () = [welcomeScreen; instructions]
+    override this.ProcessKeyboard keyInfo = None
+        
 
 type MainConsole(width, height) =
     inherit Console(width, height)
 
-    override this.Update() = ()
+    member val Scene : Scene = StartScene() :> Scene
+           with get, set
+
+    override this.Update() = this.Scene.Update()
 
     override this.Render() =
-        welcomeScreen.Render()
+        this.Scene.ConsoleObjects()
+        |> List.iter (fun entity -> entity.Render())
+
+    override this.ProcessKeyboard(keyboardInfo) = 
+        match this.Scene.ProcessKeyboard(keyboardInfo) with
+         | None -> ()
+         | Some newScene -> this.Scene <- newScene
+        true
