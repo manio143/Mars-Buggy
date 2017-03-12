b1bedbc269c4ca87c128c6cc01c04901eac713df
Marian Dziubiak <marian.dziubiak@gmail.com>
2017-03-12 11:30:31 +0100

    Rewrite Scenes more functionally

--- Game.fs
+++ Game.fs
@@ -1,51 +1,21 @@
 ﻿module Game
 
 open SadConsole.Consoles
-open SadConsole.Game
-open SadConsole.Input
 open SadConsole
 
-open ConsoleObject
-
-open Microsoft.Xna.Framework
-
-[<AbstractClass>]
-type Scene() =
-    abstract member ConsoleObjects: unit -> GameObject list
-    abstract member Update: unit -> unit
-    abstract member ProcessKeyboard: KeyboardInfo -> Scene option
-
-    default this.Update() = ()
-
-type StartScene() =
-    inherit Scene()
-
-    let welcomeScreen = 
-        createWithAnimationFromFile 20 4 "welcomeScreen"
-        <| {defaultSurface with Foreground = Color.OrangeRed}
-
-    let instructions =
-        createWithAnimation 1 24 [|"Press [Space] to start, [Q] to quit, [H] for help."|]
-        <| defaultSurface
-
-    override this.ConsoleObjects () = [welcomeScreen; instructions]
-    override this.ProcessKeyboard keyInfo = None
-        
 
 type MainConsole(width, height) =
     inherit Console(width, height)
 
-    member val Scene : Scene = StartScene() :> Scene
-           with get, set
+    let mutable Scene = Scenes.startScene
 
-    override this.Update() = this.Scene.Update()
+    let processKeyboardAndUpdate =
+        Scenes.processKeyboard (Engine.Keyboard)
+        >> Scenes.update (Engine.GameTimeElapsedUpdate)
+
+    override this.Update() =
+        Scene <- processKeyboardAndUpdate Scene
 
     override this.Render() =
-        this.Scene.ConsoleObjects()
+        Scene.ConsoleObjects
         |> List.iter (fun entity -> entity.Render())
-
-    override this.ProcessKeyboard(keyboardInfo) = 
-        match this.Scene.ProcessKeyboard(keyboardInfo) with
-         | None -> ()
-         | Some newScene -> this.Scene <- newScene
-        true
---
+++ Scenes.fs
@@ -0,0 +1,42 @@
+﻿module Scenes
+
+open SadConsole.Consoles
+open SadConsole.Game
+open SadConsole.Input
+open SadConsole
+
+open Microsoft.Xna.Framework
+
+open ConsoleObject
+
+
+type SceneType = Start | Game
+
+type Scene = {
+                ConsoleObjects: GameObject list
+                Type: SceneType
+             }
+
+let welcomeScreen = 
+    createWithAnimationFromFile 20 4 "welcomeScreen"
+    <| {defaultSurface with Foreground = Color.OrangeRed}
+
+let instructions =
+    createWithAnimation 1 24 [|"Press [Space] to start, [Q] to quit, [H] for help."|]
+    <| defaultSurface
+
+let startScene = { Type = Start; ConsoleObjects = [welcomeScreen; instructions] }
+
+let newGame() = { Type = Game; ConsoleObjects = [] }
+
+let update elapsedTime scene =
+    match scene.Type with
+    | Start -> scene
+    | Game -> scene
+
+let processKeyboard (keyboardInfo:KeyboardInfo) scene =
+    match scene.Type with
+    | Start -> 
+            if keyboardInfo.IsKeyDown(Input.Keys.Space) then newGame()
+            else scene
+    | Game -> scene
