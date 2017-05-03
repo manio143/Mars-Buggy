d89cb3ccc30cb66462633277ef3ff1fbc5856bcb
Manio <Manio@MARIAN-PC>
2017-05-04 01:44:30 +0200

    Implement basic update/keyProcess for GameScene with jumping

--- Scenes.fs
+++ Scenes.fs
@@ -81,6 +81,7 @@ module GameScene =
     let mutable speed = 500
 
     let setState newState =
+        let prevAnimationObject = currentAnimationObject
         match newState with
         | Running ->
             currentState <- Running
@@ -91,18 +92,24 @@ module GameScene =
         | Fallen ->
             currentState <- Fallen
             currentAnimationObject <- fallenBuggy
+        currentAnimationObject.Position <- prevAnimationObject.Position
 
-    let sleep (miliseconds:int) = Async.AwaitTask(Task.Delay(miliseconds))
+    let sleep (miliseconds:int) = async { do Task.Delay(miliseconds).Wait() }
 
-    let jump () = async {
+    let jump () = 
         if currentState = Running then
             setState Jumping
-            for i in 1..4 do
-                currentAnimationObject.Position <- currentAnimationObject.Position + Point(0,1)
-                do! sleep speed
+            async {
+                for i in 1..4 do
+                    currentAnimationObject.Position <- currentAnimationObject.Position + Point(0,-1)
+                    do! sleep speed
+                for i in 1..4 do
+                    currentAnimationObject.Position <- currentAnimationObject.Position + Point(0,1)
+                    do! sleep speed
+            }
         else
-            ()
-    }
+            async { do () }
+    
     
     let getScene () = {ConsoleObjects = floor::(currentAnimationObject.Objects()); Type = Game}
 
@@ -129,15 +136,35 @@ let helpSceneHandler = {
                             ProcessKeyboard = goToStart
                        }
 
+let gameUpdate delta scene =
+    List.iter (fun (o:GameObject) -> o.Update delta) scene.ConsoleObjects
+    //TODO: check collision with hole
+    GameScene.getScene()
+
+let gameProcessKeyboard = 
+    let jump kInfo scene = 
+        processSingleKey Input.Keys.Space
+        <| fun () -> GameScene.jump() |> Async.Start; GameScene.getScene()
+        <| kInfo
+        <| scene
+
+    fun kInfo ->
+        jump kInfo
+        //>> esc -> die and display score
+
+let gameSceneHandler = {
+                            Update = gameUpdate
+                            ProcessKeyboard = gameProcessKeyboard
+                       }
 
 let update elapsedTime scene =
     match scene.Type with
     | Start -> startSceneHandler.Update elapsedTime scene
-    | Game -> scene
+    | Game -> gameSceneHandler.Update elapsedTime scene
     | Help -> helpSceneHandler.Update elapsedTime scene
 
 let processKeyboard keyboardInfo scene =
     match scene.Type with
     | Start -> startSceneHandler.ProcessKeyboard keyboardInfo scene
-    | Game -> scene
+    | Game -> gameSceneHandler.ProcessKeyboard keyboardInfo scene
     | Help -> helpSceneHandler.ProcessKeyboard keyboardInfo scene
